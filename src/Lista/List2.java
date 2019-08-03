3881c987e3a8035c866c5f5a42fae3513f090bf4
Verinity <42777377+Verinity@users.noreply.github.com>
2019-08-03 11:28:23 -0300

    Create List2.java

---
+++ src/Lista/List2.java
@@ -0,0 +1,11 @@
+package Lista;
+import java.awt.datatransfer.StringSelection;
+import java.awt.Toolkit;
+public class List2 {
+	public static void main(String args[]) {
+		List Display = new List();
+		List Display2 = new List();
+		Display.DisplayOnu = ("Display ont autofind all");
+		Display.InterfaceGpon = ("interface gpon 0/");
+	}
+}
