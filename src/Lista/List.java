a6f3a9da15aa170b4eceb2ddb7e73126f343a5d5
Verinity <42777377+Verinity@users.noreply.github.com>
2019-08-03 11:26:04 -0300

    Update List.java

--- src/Lista/List.java
+++ src/Lista/List.java
@@ -1,1 +1,18 @@
 package Lista;
+import java.awt.datatransfer.StringSelection;
+import java.awt.Toolkit;
+import static java.lang.System.out;
+public class List {
+	String DisplayOnu = ("Display ont autofind all");
+	String InterfaceGpon;
+	String AddOnu;
+	String AddVlan;
+	String AddData;
+	public void Display() {
+		out.print(DisplayOnu);
+		out.print(InterfaceGpon);
+		out.print(AddOnu);
+		
+	}
+	
+}
